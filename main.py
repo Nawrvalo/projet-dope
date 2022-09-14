43807415178222e4830eb63e3c4048145967d915
Narwalo <erwan-petit@orange.fr>
2022-09-14 22:54:40 +0200

    ajout des fichiers au dépôt

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from fileinput import filename
-
-import numpy as np
-
-import matplotlib as plt
-
-import scipy as sp
-import pandas as pd
+#from fileinput import filename
+#import numpy as np
+#import matplotlib as plt
+#import pandas as pd
 
 
 def get_coordinates(line):
-    return [float(line[33:39].strip()), float(line[40:46].strip()), float(line[47:54].strip())]
-#print("Enter pdb name")
-#filename = input()
-filename = "1uz9.pdb"
+    return [float(line[31:38].strip()), float(line[39:46].strip()), float(line[47:55].strip())]
+print("Enter pdb name")
+filename = input()
 
 # Chargement des données du fichier pdb
 fn = open(filename)
@@ -21,33 +16,35 @@ CaAA = []
 CaMatrix = []
 for line in fn:
         if line.startswith("ATOM"):
-            atomline = int(line[6:11])
-            atom = line[12:16].strip()
-            Aa = line[20:23]
+            atomline = int(line[8:13])
+            atom = line[13:15].strip()
+
+            Aa = line[17:20]
             if atom == "CA":
                 CaPos.append(atomline)
-                CaMatrix.append((get_coordinates(line)))
-                CaAA.append(Aa)
+                CaMatrix.append([Aa, get_coordinates(line)])
+
 
 # Calcul des distances entre atomes CA
-CaDist = []
-y = 2
-from scipy.spatial import distance
-distance_aa = []
-i = 2
-while i < len(CaMatrix):
-    j = 0
-    while j < i-1:
-       a = CaMatrix[i]
-       b = CaMatrix[j]
-       distance_aa.append([CaAA[i], CaAA[j], round(distance.euclidean(a, b), 2)])
-       j += 1
-    i+=1
 
-"""print("Affichage Matrice Distance_aa")
-for x in distance_aa:
-    print(x)
-"""
+def calcul_dist(CaMatrix):
+    CaDist = []
+    y = 2
+    from scipy.spatial import distance
+    distance_aa = []
+    i = 2
+    while i < len(CaMatrix):
+        j = 0
+        while j < i-1:
+            a = CaMatrix[i][1]
+            b = CaMatrix[j][1]
+            distance_aa.append([CaMatrix[i][0], CaMatrix[j][0], round(distance.euclidean(a, b), 2)])
+            j += 1
+        i+=1
+    return distance_aa
+
+distance_aa = calcul_dist(CaMatrix)
+
 
 # Chargement du fichier dope
 dope = []
@@ -56,40 +53,50 @@ for line in dop:
     if line[4:6] == "CA" and line[11:13] == "CA":
         dope.append(line.split())
 
-#print("Affichage Matrice dope")
-#for x in dope:
-#    print(x)
+
 
 # Pour chaque distance entre Carbones Alpha de même Acide Aminé, cumul des énergies trouvées dans DOPE
-ener = []
-for x in  distance_aa:
-    i = round(x[2]*2, 0) + 4
-    if i < 34:
-      for y in dope:
+def calcul_energ(distance_aa):
+    ener = []
+    for x in  distance_aa:
+        i = round(x[2]*2, 0) + 4
+        if i < 34:
+            for y in dope:
+                if x[0] == y[0] and x[1] == y[2]:
+                   ener.append(float(y[int(i)]))
 
-          if x[0] == y[0] and x[1] == y[2]:
-             #print(f"x[0] = {x[0]}, y[0] = {y[0]}, x[1] = {x[1]}, y[2] = {y[2]}")
-             ener.append(float(y[int(i)]))
-# Somme des énergies cumulées
-energiesCumulees = sum(ener)
-print(energiesCumulees)
-# Recherche aléatoire d'énergies dans DOPE
+    # Somme des énergies cumulées
+    energiesCumulees = sum(ener)
+    return energiesCumulees
+
+# Appel fonction calcul_energ
+energiesCumulees = calcul_energ(distance_aa)
+print(f"energies cumulées = {energiesCumulees}")
+
+
+# Recherche d'énergies issues de conformations aléatoires de la protéine de base
+
+print("Calcul du z-score")
 import random
-temoin = []
+t = 0
+RandEner = []
+while t < 30:
+    t += 1
+    random.shuffle(CaMatrix)
+    distance_aa = calcul_dist(CaMatrix)
+    RandEner.append(calcul_energ(distance_aa))
 
-b = 0
-while b < 10:
-  temoin.clear()
-  a = 0
-  while a < 100:
-      i = random.randrange(0, 100)
-      j = random.randrange(4, 34)
-      temoin.append(float(dope[i][j]))
-      a += 1
-  c = sum(temoin)
-  print(c)
-  b += 1
+
 # Calcul du z-score
+
+
+import statistics
+mu = round(statistics.mean(RandEner), 2)
+ety = round(statistics.pstdev(RandEner), 2)
+z_score = (energiesCumulees - mu) / ety
+print(f"z_score = {z_score}")
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projet-Dope
+## Setup your environment
+Clone the repistory:
+| git clone https://github.com/Nawrvalo/projet-dope.git |
+
